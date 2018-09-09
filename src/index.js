90fb0e153362d97845c2f39e956ca426a1758556
KuTuGu <256317135@qq.com>
2018-09-10 00:42:11 +0800

    fix: Modify compatibility issues

--- src/index.js
+++ src/index.js
@@ -34,15 +34,26 @@ function birdMove(proportion) {
 }
 
 function touchStart(event){
+    /* 禁止浏览器缩放 */
+    if(event.touches[1]){
+        window.event.preventDefault();
+        return false;
+    }
+
     let finger = event.touches[0];
 
     currentY = preY = finger.pageY;
 
-    event.target.addEventListener('touchmove', touchMove, {passive: true});
-    event.target.addEventListener('touchcancel', touchCancel, {passive: true});
+    event.target.addEventListener('touchmove', touchMove, false);
+    event.target.addEventListener('touchcancel', touchCancel, false);
 }
 
 function touchMove(event){
+    if(event.touches[1]){
+        window.event.preventDefault();
+        return false;
+    }
+
     let finger = event.touches[0];
 
     window.requestAnimationFrame(()=>{
@@ -52,7 +63,7 @@ function touchMove(event){
             imageX = image.style.transform.match(patt)[1],
             trans;
 
-        if((moveY < 0 && parseInt(imageX) === 0) || (moveY > 0 && parseInt(imageX) === -moveWidth)) 
+        if((moveY < 0 && parseInt(imageX) === 0) || (moveY > 0 && !(parseInt(imageX) > -moveWidth)))
             return false;
 
         birdMove(moveY / moveWidth);
@@ -62,7 +73,7 @@ function touchMove(event){
         if(imageX - moveY > 0)
             trans = 0;
 
-        else if(moveY - imageX >= moveWidth)
+        else if(!(moveY - imageX < moveWidth))
             trans = -moveWidth;
 
         else
@@ -71,42 +82,52 @@ function touchMove(event){
         image.style.transform = "translate(" + trans + "px, 0)";
         image.style.webkitTransform = "translate(" + trans + "px, 0)";
     })
+
+    event.preventDefault();
 }
 
 function touchCancel(event){
+    if(event.touches[1]){
+        window.event.preventDefault();
+        return false;
+    }
+
     let finger = event.touches[0];
 
     preY = currentY = finger.pageY;
 }
 
-// 配置位移矢量 一共走100步
-const STEP = 100;
-let curStep = 0;
-
-const path = document.getElementById('motionPath');
-const bird = document.getElementById('crayImg');
-const image = document.getElementsByTagName('img')[0];
-image.style.transform = "translate(0, 0)";
-
-// 最重要的两个属性  获取长度，以及每个点的坐标
-const totalLength = path.getTotalLength();
-const initPosition = path.getPointAtLength(0);
-
 //正则匹配，获取偏移
 const patt = new RegExp(/translate\((-?\d+)(?:px)/,"i");
-
-//背景图片长度，鸟坐标位置记录，当前移动的路径长度
-const moveWidth = image.width - window.innerWidth;
-let prePosition = new vector(initPosition.x, initPosition.y);
-let curPosition = new vector(initPosition.x, initPosition.y);
-let movedLength = 0;
-
-// 定位bird 
-bird.style.left = curPosition.x + 'px';
-bird.style.top = curPosition.y + 'px';
+//固定水平向量
 const birdVector = new vector(-1, 0);
 
+//获取路径长度，以及初始路径坐标
+let totalLength, initPosition;
+
+//获取路径点坐标，标记已移动距离
+let prePosition, curPosition;
+let movedLength = 0;
+
+let path, bird, image, moveWidth;
 //手指触点位置
 let preY, currentY;
 
-document.addEventListener('touchstart', touchStart);
+window.onload = () => {
+    path = document.getElementById('motionPath');
+    bird = document.getElementById('crayImg');
+    image = document.getElementsByTagName('img')[0];
+
+    image.style.transform = "translate(0, 0)";
+    moveWidth = image.width - window.innerWidth;
+
+    totalLength = path.getTotalLength(); 
+    initPosition = path.getPointAtLength(0);
+    curPosition = prePosition = new vector(initPosition.x, initPosition.y);
+
+    // 定位bird 
+    bird.style.left = curPosition.x + 'px';
+    bird.style.top = curPosition.y + 'px';
+
+    document.addEventListener('touchstart', touchStart, false);
+}
